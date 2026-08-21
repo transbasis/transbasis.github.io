b29cca0cd057ce8c8566b8428f9e16a7cd97cdad
transbasis <transbasis@gmail.com>
2026-08-21 09:38:00 +0900

    feat: initialize jekyll site

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Manage blog";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          ruby_3_3
+          bundler
+        ];
+      };
+
+    };
+}
